b8250faf609c4cb8eda7896e2c8cba3a46d1a639
Kelveen Raj <kelveenrajv@gmail.com>
2025-06-22 11:28:19 +0800

    week 8

--- week-7/index.js
+++ week-7/index.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("btn-1");
 const box = document.getElementById("box");
+const pokemonContainer = document.getElementById("pokemon")
 
 const generateColour = () => {
   const colours = ["red", "green", "blue"];
@@ -25,3 +26,23 @@ box.addEventListener("mouseover", growBox);
 box.addEventListener("mouseout", shrinkBox);
 box.addEventListener("click", generateColour);
 
+// Calling API
+
+const API_URL = "https://pokeapi.co/api/v2/pokemon/"; // this is the endpoint we call
+
+const getPokemonData = async () => {
+  const response = await fetch(API_URL); // forcing JS to wait for an ction to be done
+
+  const data = await response.json()// Convert response into object
+
+  const pokemonName = data.results[0].name
+
+  pokemonContainer.textContent = pokemonName
+
+
+  console.log(data.results[0].name); 
+};
+
+getPokemonData()
+
+// fetch method to call api
